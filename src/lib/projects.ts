d11b37b2d66a4865e0e5e8370d56248a59226b08
vip3r <146040029+qvipin@users.noreply.github.com>
2024-01-29 18:49:25 -0500

    uhh plz work idek if this is correct lol

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,76 +1,70 @@
 import emojiRegex from 'emoji-regex';
 import { log } from 'next-axiom';
 
-import type { GitHubRepos, Project, ProjectPost } from '~/types';
+import type { GitHubRepos, Project, ProjectOverride } from '~/types';
 
-/**
- * Fetch Projects
- *
- * Make a GET request to the GitHub API to gather all repositories
- * under my `nurodev` username & then filter them down to only
- * include those that contain the `portfolio` topic
- *
- * @TODO Switch to v3 API using GraphQL to save over-fetching
- */
 export async function fetchProjects(): Promise<Array<Project> | null> {
-	const response = await fetch('https://api.github.com/users/qvipin/repos', {
-		headers: {
-			...(process.env.GITHUB_PAT && {
-				authorization: `token ${process.env.GITHUB_PAT}`,
-			}),
-		},
-	});
-	if (response.status !== 200) {
-		const json = (await response.json()) as {
-			documentation_url: string;
-			message: string;
-		};
+	const user = 'qvipin';
 
-		console.error({ error: json });
-		log.error('Failed to fetch projects', {
-			error: json,
+	let repos: GitHubRepos = [];
+	let page = 1;
+	while (true) {
+		const response = await fetch(`https://api.github.com/users/${user}/starred?per_page=100&page=${page}`, {
+			headers: {
+				...(process.env.GITHUB_PAT && {
+					authorization: `token ${process.env.GITHUB_PAT}`,
+				}),
+			},
 		});
 
-		return null;
+		const res = await response.json();
+
+		if (response.status !== 200) {
+			const error = res as {
+				documentation_url: string;
+				message: string;
+			};
+
+			console.error({ error });
+			log.error('Failed to fetch repos', { error });
+
+			return null;
+		}
+
+		if (res.length === 0) break;
+		repos = repos.concat(res as GitHubRepos);
+		page += 1;
 	}
 
-	const json = (await response.json()) as GitHubRepos;
+	const { default: rawProjectOverrides } = await import('~/data/projects.json');
+	const projectOverrides = rawProjectOverrides as Array<ProjectOverride>;
 
-	const { default: rawProjectPosts } = await import('~/data/projects.json');
-	const projectPosts = rawProjectPosts as Array<ProjectPost>;
-
-	const projects: Array<Project> = json
+	const projects: Array<Project> = repos
+		.sort((a, b) => b.stargazers_count - a.stargazers_count)
 		.map((repo) => {
 			if (!repo.topics.includes('project')) return null;
 
-			if (repo.archived) return null;
-
-			// Strip the emoji suffix from the repo description
-			const trimmedDescription = repo.description.split(' ');
-			trimmedDescription.shift();
-			const description = trimmedDescription.join(' ');
-
-			// Check if there is a matching blog post to attach
-			const repoPost =
-				projectPosts.length > 0 &&
-				projectPosts.find(
-					(post) => post.repository.toLowerCase() === repo.full_name.toLowerCase(),
+			// Check if there is a matching details override
+			const projectOverride =
+				projectOverrides.length > 0 &&
+				projectOverrides.find(
+					(override) => override.repository.toLowerCase() === repo.full_name.toLowerCase(),
 				);
+			let description = projectOverride ? projectOverride.description : repo.description;
+
+			if (!description) return null;
+			const [emoji, ...desc] = description.split(' ');
+			description = desc.join(' ');
+			if (!emojiRegex().test(emoji)) return null;
+			if (repo.owner.login === user && !repo.topics.includes('in-portfolio')) return null;
 
 			return {
 				description,
-				icon: ((): string => {
-					if (!repo.description) return undefined;
-
-					const char = repo.description.split(' ')[0];
-
-					return emojiRegex().test(char) ? char : undefined;
-				})(),
+				icon: emoji,
 				homepage: repo.homepage ?? undefined,
 				name: repo.name,
-				post: repoPost ? `/blog/${repoPost.post}` : undefined,
 				template: false,
-				url: repo.html_url.toLowerCase(),
+				url: repo.html_url,
 			} as Project;
 		})
 		.filter((project) => project !== null);
